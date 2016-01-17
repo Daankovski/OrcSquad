44da964a7a36e293d7cff10ca97f92afed8a1aa0
Daankovski <danielepke@hotmail.com>
2016-01-17 21:23:46 +0100

    First pieces of art implemented

---
+++ Assets/_Scripts/BaseCharacter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseCharacter : MonoBehaviour {
+
+    private float f_baseLives;
+    private float f_baseMana;
+    private float f_baseMovementSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+        f_baseLives = 100f;
+        f_baseMana = 100f;
+        f_baseMovementSpeed = 5f;
+	}
+
+    // Getters & Setters
+    public float BaseLives {
+        get { return f_baseLives; }
+    }
+
+    public float BaseMana {
+        get { return f_baseMana; }
+    }
+
+    public float BaseMovementSpeed {
+        get { return f_baseMovementSpeed; }
+    }
+}
---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    private BaseCharacter bc;
+
+    private bool isFacingRight = true;
+    private bool canJump = false;
+
+    private float f_Health;
+    private float f_Mana;
+    private float f_MovementSpeed;
+
+	
+	void Start () {
+        bc = GetComponent<BaseCharacter>();
+	}
+	
+	
+	void Update () {
+	
+	}
+}
